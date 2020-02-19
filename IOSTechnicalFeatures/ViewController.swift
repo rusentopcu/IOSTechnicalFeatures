4527524748acaa7c89cac89580026f5634821d24
Rusen Topcu <rusentopcu@msn.com>
2020-02-19 09:56:29 +0300

    Project created

---
+++ IOSTechnicalFeatures/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  IOSTechnicalFeatures
+//
+//  Created by Rusen Topcu on 19.02.2020.
+//  Copyright © 2020 Rusen Topcu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
