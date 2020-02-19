d77cd9ca3255936090f6c30b7ae919c6ddedd256
Rusen Topcu <rusentopcu@msn.com>
2020-02-19 12:21:33 +0300

    Face Recognition

--- IOSTechnicalFeatures/ViewController.swift
+++ IOSTechnicalFeatures/ViewController.swift
@@ -7,16 +7,21 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
     //MARK: - Outlets
     @IBOutlet weak var changeButton: UIButton!
+    @IBOutlet weak var myLabel: UILabel!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //MARK: - View Controller'da hangi mode'un kullanılacağını belirliyoruz. Seçime bakılmaksızın Şart haline getirir.(info.plist: User Interface Style Dark yada Light ile de güncelleyebiliriz).
-        overrideUserInterfaceStyle = .dark
+        overrideUserInterfaceStyle = .light
+        
     
     }
 
@@ -33,5 +38,28 @@ class ViewController: UIViewController {
     }
     
     
+    @IBAction func signButton(_ sender: Any) {
+        //Authentication için LAContext'ten nesne üreteceğiz bunun için önce LocalAuthentication import edilmelidir. Ayrıca info.plist'te Privacy-Face ID Usage Description girilmelidir.
+        
+        let authContex = LAContext()
+        var error: NSError?
+        if authContex.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            authContex.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "FaceID true?") { (success, error) in
+                if success == true {
+                DispatchQueue.main.async {
+                    self.performSegue(withIdentifier: "toSecondVC", sender: nil)
+                  }
+                } else {
+                    DispatchQueue.main.async {
+                        self.myLabel.text = "Error!"
+                    }
+                    
+                }
+            }
+        }
+        
+    }
 }
+
+
 
