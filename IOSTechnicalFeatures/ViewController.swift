ed609636fad235271bc86eb529202c0847efeccb
Rusen Topcu <rusentopcu@msn.com>
2020-02-19 10:27:17 +0300

    Dark Mode

--- IOSTechnicalFeatures/ViewController.swift
+++ IOSTechnicalFeatures/ViewController.swift
@@ -9,13 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: - Outlets
+    @IBOutlet weak var changeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        //MARK: - View Controller'da hangi mode'un kullanılacağını belirliyoruz. Seçime bakılmaksızın Şart haline getirir.(info.plist: User Interface Style Dark yada Light ile de güncelleyebiliriz).
+        overrideUserInterfaceStyle = .dark
+    
     }
 
 
+    //MARK: - Uygulama açıkken kullanıcının yaptığı mode değişikliğini algılaması için (Dark,Light) bu func. kullanıldı.
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+            let userInterfaceStyle = traitCollection.userInterfaceStyle
+            if userInterfaceStyle == .dark {
+                changeButton.tintColor = UIColor.white
+            }
+            else {
+                changeButton.tintColor = UIColor.blue
+            }
+    }
+    
+    
 }
 
